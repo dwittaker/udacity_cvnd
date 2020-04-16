496b2d6ad80a95af87813a5649675b7c87fbec7b
dwittaker <dwittaker@gmail.com>
2020-04-16 06:24:40 -0500

    Developed model

--- P1/models.py
+++ P1/models.py
@@ -19,7 +19,22 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 5) #220
+        self.conv1_bn = nn.BatchNorm2d(32)
+        self.maxpool1 = nn.MaxPool2d(2,2) #110
+        self.conv2 = nn.Conv2d(32, 64, 3) #108
+        self.conv2_bn = nn.BatchNorm2d(64)
+        self.maxpool2 = nn.MaxPool2d(2,2) #54
+        
+        self.conv3 = nn.Conv2d(64, 128, 3) #52
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.maxpool3 = nn.MaxPool2d(2,2) #26
+        
+        self.fc1 = nn.Linear(128*26*26, 408)
+        self.drop1 = nn.Dropout2d(p=0.2)
+        self.fc2 = nn.Linear(408, 136)
+        
+        
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -30,6 +45,13 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.maxpool1(F.relu(self.conv1_bn(self.conv1(x))))
+        x = self.maxpool2(F.relu(self.conv2_bn(self.conv2(x))))
+        x = self.maxpool3(F.relu(self.conv3_bn(self.conv3(x))))
+        x = x.view(x.size(0), -1)
+        
+        x = self.drop1(self.fc1(x))
+        x = self.fc2(x)
         
         
         # a modified x, having gone through all the layers of your model, should be returned
